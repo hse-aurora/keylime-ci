1104df91540f1f63151dfefc7ee4b4fce24d114e
Jean Snyman <jean.snyman@hpe.com>
2023-06-12 11:25:23 +0100

    Remove delay before creating agent container
    Commit 1ca3248 introduced logic to wait for the keylime_verifier and
    keylime_registrar containers to be available before creating the
    keylime_agent container. This was in an attempt to stop tcp connect
    errors from being printed to the agent's logs before the registrar is
    reachable. However, upon further investigation, these errors do not
    appear when the containers are first created but rather when the Docker
    daemon is restarted (e.g., after a system reboot or when a new GCE VM
    is instantiated from a GCE image). As such, this commit reverts this
    change.

--- helpers/install-containers.sh
+++ helpers/install-containers.sh
@@ -29,17 +29,7 @@ sudo docker pull "gcr.io/project-keylime/keylime_agent:$A_TAG"
 sudo docker network create keylime-net
 
 # Create containers from the images with the appropriate mappings to allow sharing of data between containers
-verifier_id=$(sudo docker run -itd -v kl-data-vol:/var/lib/keylime -v kl-vrt-config-vol:/etc/keylime -v kl-vrt-src-vol:/usr/local/src/keylime --net keylime-net -p 8880:8880 -p 8881:8881 --restart unless-stopped --name keylime_verifier "gcr.io/project-keylime/keylime_verifier:$VRT_TAG")
-registrar_id=$(sudo docker run -itd -v kl-data-vol:/var/lib/keylime -v kl-vrt-config-vol:/etc/keylime -v kl-vrt-src-vol:/usr/local/src/keylime --net keylime-net -p 8890:8890 -p 8891:8891 --restart unless-stopped --name keylime_registrar "gcr.io/project-keylime/keylime_registrar:$VRT_TAG")
+sudo docker run -itd -v kl-data-vol:/var/lib/keylime -v kl-vrt-config-vol:/etc/keylime -v kl-vrt-src-vol:/usr/local/src/keylime --net keylime-net -p 8880:8880 -p 8881:8881 --restart unless-stopped --name keylime_verifier "gcr.io/project-keylime/keylime_verifier:$VRT_TAG"
+sudo docker run -itd -v kl-data-vol:/var/lib/keylime -v kl-vrt-config-vol:/etc/keylime -v kl-vrt-src-vol:/usr/local/src/keylime --net keylime-net -p 8890:8890 -p 8891:8891 --restart unless-stopped --name keylime_registrar "gcr.io/project-keylime/keylime_registrar:$VRT_TAG"
 sudo docker run -itd -v kl-data-vol:/var/lib/keylime -v kl-vrt-config-vol:/etc/keylime -v kl-vrt-src-vol:/usr/local/src/keylime --net keylime-net --restart unless-stopped --entrypoint /bin/bash --name keylime_tenant "gcr.io/project-keylime/keylime_tenant:$VRT_TAG"
-
-# Wait up to 30s for verifier and registrar to start
-count=0
-while ! docker top "$verifier_id" &> /dev/null || ! docker top "$registrar_id" &> /dev/null; do
-  sleep 1
-  let "++count"
-
-  [[ $count -ge 30 ]] && break
-done
-
 sudo docker run -itd -v kl-data-vol:/var/lib/keylime -v kl-a-config-vol:/etc/keylime -v kl-a-src-vol:/usr/local/src/rust-keylime --net keylime-net -p 9002:9002 --restart unless-stopped --tmpfs /var/lib/keylime/secure:size=1024k,mode=0700 --device /dev/tpm0:/dev/tpm0 --device /dev/tpmrm0:/dev/tpmrm0 --name keylime_agent "gcr.io/project-keylime/keylime_agent:$A_TAG"
